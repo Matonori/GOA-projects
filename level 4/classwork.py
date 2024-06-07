ae46ef8e68e6147c43a82a9b26fcedabc4547593
Matonori <papidzegiorgi66@gmail.com>
2024-06-07 22:30:50 +0400

    third commit

---
+++ level 4/classwork.py
@@ -0,0 +1,41 @@
+#"declaration" - creating a variable
+#"statement" - command (ბრძანება)
+#"numerical data" - data containing numbers
+#"contantenent" - adding 2 numbers to each other
+
+#int() - turns into integral
+#str() - turns into string
+
+# print(3 + 5)
+# number = 3 + 5
+# print(number)
+# ctrl + question mark button makes highlighted commands have a # at the start
+
+# num = True
+# print(num)
+# print(int(num))
+#an example of using int
+
+#incrementation - increasing by 1
+
+# reassing - მნიშვნელობის გადაწერა. giving a variable a different value
+# debugging - fixing errors and bugs in your code.
+# w3schools.com - place to check commands and stuff
+
+
+name = "data"
+surname = "diasamidze"
+age = 100
+
+print("My name is" + " " + name + "my surname is" + " " + surname + "my age is" + " " + str(age))
+
+
+#rassign - მნიშვნელობის გადაწერა. giving a variable a different value.
+num1 = 5
+num = 5
+print(num + 1)
+
+balance = 10
+discount = 5
+print(balance - discount)
+finalprice = 5
---
+++ level 4/homework.py
@@ -0,0 +1,38 @@
+from turtle import *
+#-------all the cones----------
+
+#cone 1
+speed(1)
+width(5)
+left(60)
+forward(50)
+right(115)
+forward(50)
+penup()
+
+#cone 2
+goto(0, -70)
+pendown()
+left(105)
+forward(120)
+right(100)
+forward(120)
+penup()
+
+#cone 3
+goto(120, 65)
+pendown()
+left(105)
+forward(40)
+right(105)
+forward(40)
+penup()
+
+#cone 4
+goto(100, 40)
+
+
+
+
+
+
